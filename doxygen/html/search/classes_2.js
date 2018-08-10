5943fef38e165fd03f0531a5ddaf8e18135abf76
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2018-08-10 23:43:52 +0200

    update doxygen

--- doxygen/html/search/classes_2.js
+++ doxygen/html/search/classes_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['divfunctor',['DivFunctor',['../classDivFunctor.html',1,'']]],
-  ['dopri',['DOPRI',['../structDOPRI.html',1,'']]],
+  ['dopri54',['DOPRI54',['../structDOPRI54.html',1,'']]],
   ['dopri87',['DOPRI87',['../structDOPRI87.html',1,'']]]
 ];
