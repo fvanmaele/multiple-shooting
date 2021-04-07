4312183901efd39f090b31e0a54ecc5443c000e4
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2021-04-07 14:41:12 +0200

    remove generated documentation

--- doxygen/html/search/all_1.js
+++ doxygen/html/search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['convergencetester',['ConvergenceTester',['../classConvergenceTester.html',1,'']]],
   ['curve',['Curve',['../classCurve.html',1,'']]],
   ['curvestoer',['CurveStoer',['../classTest_1_1CurveStoer.html',1,'Test']]],
   ['curvetf',['CurveTF',['../classCurveTF.html',1,'']]],
